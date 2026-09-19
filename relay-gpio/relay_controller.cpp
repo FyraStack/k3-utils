4afd19175d14622acd3f674dc4fbb3983f56fb5e
Cypress Reed <cypress@fyralabs.com>
2026-09-19 01:53:01 +0000

    newer version of API actually

--- relay-gpio/relay_controller.cpp
+++ relay-gpio/relay_controller.cpp
@@ -4,29 +4,39 @@
 
 #include <stdexcept>
 
-RelayController::RelayController() :
-    chip_(gpiod::chip(configuration::gpio_chip())),
-    active_low_(configuration::environment_int("RELAY_ACTIVE_LOW", 1) != 0), offsets_(configuration::relay_offsets()) {
-    for (std::size_t i = 0; i < relay_count; ++i) {
-        lines_[i] = chip_.get_line(offsets_[i]);
-        lines_[i].request({"relay-gpio", gpiod::line_request::DIRECTION_OUTPUT, 0}, off_value());
-    }
-}
+RelayController::RelayController()
+    : chip_(gpiod::chip(configuration::gpio_chip())),
+      active_low_(configuration::environment_int("RELAY_ACTIVE_LOW", 1) != 0),
+      offsets_(configuration::relay_offsets()), request_(make_request()) {}
 
 RelayController::~RelayController() {
-    for (auto &line: lines_) {
-        if (line) {
-            line.release();
-        }
-    }
+  if (request_) {
+    request_.release();
+  }
+}
+
+gpiod::line_request RelayController::make_request() {
+  gpiod::line::offsets offsets(offsets_.begin(), offsets_.end());
+  gpiod::line_settings settings;
+  settings.set_direction(gpiod::line::direction::OUTPUT)
+      .set_output_value(off_value() ? gpiod::line::value::ACTIVE
+                                    : gpiod::line::value::INACTIVE);
+
+  auto builder = chip_.prepare_request();
+  builder.set_consumer("relay-gpio").add_line_settings(offsets, settings);
+  return builder.do_request();
 }
 
 void RelayController::set(std::size_t index, bool on) {
-    if (index >= relay_count) {
-        throw std::out_of_range("relay index");
-    }
-    lines_[index].set_value(on ? on_value() : off_value());
-    state_[index] = on;
+  if (index >= relay_count) {
+    throw std::out_of_range("relay index");
+  }
+  request_.set_value(offsets_[index],
+                     on ? (on_value() ? gpiod::line::value::ACTIVE
+                                      : gpiod::line::value::INACTIVE)
+                        : (off_value() ? gpiod::line::value::ACTIVE
+                                       : gpiod::line::value::INACTIVE));
+  state_[index] = on;
 }
 
 bool RelayController::get(std::size_t index) const { return state_.at(index); }
